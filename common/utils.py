c371374ad26ee4393190bb29d073a48d60a07599
Gimmyhat <nikitchenko.as@gmail.com>
2023-01-09 15:52:42 +0800

    * copy lesson_10 to main working tree

--- common/utils.py
+++ common/utils.py
@@ -1,29 +1,35 @@
 import json
-from .constants import MAX_PACKAGE_LENGTH, ENCODING
+import sys
+
+from common.variables import *
+from decors import log
+from errors import IncorrectDataRecivedError, NonDictInputError
+
+sys.path.append('../')
 
 
+# Утилита приёма и декодирования сообщения
+# принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
+@log
 def get_message(client):
-    """ receiving message
-    :param client:
-    :return
-    """
-    response_bytes = client.recv(MAX_PACKAGE_LENGTH)
-    if isinstance(response_bytes, bytes):
-        json_response = response_bytes.decode(ENCODING)
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
         response = json.loads(json_response)
         if isinstance(response, dict):
             return response
-        raise ValueError
-    raise ValueError
+        else:
+            raise IncorrectDataRecivedError
+    else:
+        raise IncorrectDataRecivedError
 
 
+# Утилита кодирования и отправки сообщения
+# принимает словарь и отправляет его
+@log
 def send_message(sock, message):
-    """
-    sending messages
-    :param sock:
-    :param message:
-    :return:
-    """
-    json_message = json.dumps(message)
-    message_bytes = json_message.encode(ENCODING)
-    sock.send(message_bytes)
+    if not isinstance(message, dict):
+        raise NonDictInputError
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
--- decors.py
+++ decors.py
@@ -1,22 +1,20 @@
-import logging
 import sys
-import traceback
+import logs.config_server_log
+import logs.config_client_log
+import logging
 
+# метод определения модуля, источника запуска.
 if sys.argv[0].find('client') == -1:
-    LOGGER = logging.getLogger('server_logger')
+    #если не клиент то сервер!
+    logger = logging.getLogger('server')
 else:
-    LOGGER = logging.getLogger('client_logger')
+    # ну, раз не сервер, то клиент
+    logger = logging.getLogger('client')
 
 
-class Log:
-    def __call__(self, func):
-        def create_log(*args, **kwargs):
-            res = func(*args, **kwargs)
-            LOGGER.debug(
-                f'Called function {func.__name__} with params {args, kwargs}. '
-                f'From module {func.__module__}. '
-                f'From function {traceback.format_stack()[0].strip().split()[-1]}. ', stacklevel=2
-            )
-            return res
-
-        return create_log
+def log(func_to_log):
+    def log_saver(*args , **kwargs):
+        logger.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args} , {kwargs}. Вызов из модуля {func_to_log.__module__}')
+        ret = func_to_log(*args , **kwargs)
+        return ret
+    return log_saver
